460712ff2b23998a27dece7da2c0b3b2fa074064
MatheusCod <matheus.log100@gmail.com>
2020-10-17 14:43:15 -0300

    Update text10x.sh

--- test10x.sh
+++ test10x.sh
@@ -6,13 +6,13 @@ if [ "$#" -ne 1 ]; then
 fi
 
 if [ "$1" = "pw9" ]; then
-  mkdir -p PW9/convolu_results
+  sudo mkdir -p $PWD/PW9/convolu_results
   for i in {0..9}; do
     sudo sync;sudo bash -c "echo 3 > /proc/sys/vm/drop_caches"
     perf stat -o PW9/N${i}_PW9_convolu_results.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions,idle-cycles-backend,idle-cycles-frontend python3 rede_neural_convolucional.py
   done
 
-  mkdir -p PW9/image_results
+  sudo mkdir -p $PWD/PW9/image_results
   for i in {0..9}; do
     sudo sync;sudo bash -c "echo 3 > /proc/sys/vm/drop_caches"
     perf stat -o PW9/N${i}_PW9_image_results.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions,idle-cycles-backend,idle-cycles-frontend python3 image_classification.py
@@ -23,13 +23,13 @@ if [ "$1" = "pw9" ]; then
 fi
 
 if [ "$1" = "pw8" ]; then
-  mkdir -p PW8/convolu_results
+  sudo mkdir -p $PWD/PW8/convolu_results
   for i in {0..9}; do
     sudo sync;sudo bash -c "echo 3 > /proc/sys/vm/drop_caches"
     perf stat -o PW8/N${i}_PW8_convolu_results.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions python3 rede_neural_convolucional.py
   done
 
-  mkdir -p PW8/image_results
+  sudo mkdir -p $PWD/PW8/image_results
   for i in {0..9}; do
     sudo sync;sudo bash -c "echo 3 > /proc/sys/vm/drop_caches"
     perf stat -o PW8/N${i}_PW8_image_results.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions python3 image_classification.py
