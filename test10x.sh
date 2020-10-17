ccb66ee47445e112721b46f161b44de7213b1cdb
MatheusCod <matheus.log100@gmail.com>
2020-10-17 14:26:49 -0300

    Update text10x.sh

--- test10x.sh
+++ test10x.sh
@@ -11,14 +11,16 @@ if [ "$#" -ne 1 ]; then
 fi
 
 if [ "$1" = "pw9" ]; then
+  mkdir -p PW9/convolu_results
   for i in {0..9}; do
     sync; echo 3 > /proc/sys/vm/drop_caches
-    perf stat -o PW9/N${i}_PW9_results_convolu.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions,idle-cycles-backend,idle-cycles-frontend python3 rede_neural_convolucional.py
+    perf stat -o PW9/N${i}_PW9_convolu_results.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions,idle-cycles-backend,idle-cycles-frontend python3 rede_neural_convolucional.py
   done
 
+  mkdir -p PW9/image_results
   for i in {0..9}; do
     sync; echo 3 > /proc/sys/vm/drop_caches
-    perf stat -o PW9/N${i}_PW9_results_image.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions,idle-cycles-backend,idle-cycles-frontend python3 image_classification.py
+    perf stat -o PW9/N${i}_PW9_image_results.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions,idle-cycles-backend,idle-cycles-frontend python3 image_classification.py
   done
 
   echo "Tests for POWER9 completed."
@@ -26,14 +28,16 @@ if [ "$1" = "pw9" ]; then
 fi
 
 if [ "$1" = "pw8" ]; then
+  mkdir -p PW8/convolu_results
   for i in {0..9}; do
     sync; echo 3 > /proc/sys/vm/drop_caches
-    perf stat -o PW8/N${i}_PW8_results_convolu.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions python3 rede_neural_convolucional.py
+    perf stat -o PW8/N${i}_PW8_convolu_results.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions python3 rede_neural_convolucional.py
   done
 
+  mkdir -p PW8/image_results
   for i in {0..9}; do
     sync; echo 3 > /proc/sys/vm/drop_caches
-    perf stat -o PW8/N${i}_PW8_results_image.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions python3 image_classification.py
+    perf stat -o PW8/N${i}_PW8_image_results.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions python3 image_classification.py
   done
 
   echo "Tests for POWER8 completed."
