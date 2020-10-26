beae76d907f7e21d411904753382b4aa93443014
MatheusCod <matheus.log100@gmail.com>
2020-10-26 21:36:03 +0000

    hotdog

--- ResNet/test10x.sh
+++ ResNet/test10x.sh
@@ -7,9 +7,10 @@ fi
 
 if [ "$1" = "pw9" ]; then
   mkdir -p ~/PW9/ResNet_hotdog
+  mkdir -p ~/PW9/ResNet_hotdog/acc
   for i in {0..9}; do
     sudo sync;sudo bash -c "echo 3 > /proc/sys/vm/drop_caches"
-    perf stat -o ~/PW9/ResNet_hotdog/N${i}_PW9_ResNet_hotdog.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions,idle-cycles-backend,idle-cycles-frontend python3 resnetClassification.py >> acc_hotdog${i}.txt
+    perf stat -o ~/PW9/ResNet_hotdog/N${i}_PW9_ResNet_hotdog.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions,idle-cycles-backend,idle-cycles-frontend python3 resnetClassification.py >> ~/PW9/ResNet_hotdog/acc/PW9acc_hotdog${i}.txt
   done
   
   echo "Tests for POWER9 completed."
@@ -18,9 +19,10 @@ fi
 
 if [ "$1" = "pw8" ]; then
   mkdir -p ~/PW8/ResNet_hotdog
+  mkdir -p ~/PW8/ResNet_hotdog/acc
   for i in {0..9}; do
     sudo sync;sudo bash -c "echo 3 > /proc/sys/vm/drop_caches"
-    perf stat -o ~/PW8/ResNet_hotdog/N${i}_PW8_ResNet_hotdog.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions python3 resnetClassification.py >> acc_hotdog${i}.txt
+    perf stat -o ~/PW8/ResNet_hotdog/N${i}_PW8_ResNet_hotdog.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions python3 resnetClassification.py >> ~/PW8/ResNet_hotdog/acc/PW8_acc_hotdog${i}.txt
   done
 
   echo "Tests for POWER8 completed."
