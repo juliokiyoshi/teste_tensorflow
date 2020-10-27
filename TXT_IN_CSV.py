94c2f8dff0ea03de45cb59bc9814027fe32ae44f
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-10-27 10:40:09 -0300

    Update TXT_IN_CSV.py

--- TXT_IN_CSV.py
+++ TXT_IN_CSV.py
@@ -29,17 +29,24 @@ def append_list_as_row(file_name, list_of_elem):
 append_list_as_row(file_output, primeira_linha)
 
 for j in range(0,9):
-    file_name='N'+str(j)+args.name[2:] # POR EXEMPLO O NOME DO TXT ONDE ESTÁ OS DADOS É N7_PW8_vgg16.txt, então NOME_DO_ARQUIVO == _PW8_vgg16.txt
+    file_name= 'N' + str(j) + args.name[2:] # POR EXEMPLO O NOME DO TXT ONDE ESTÁ OS DADOS É N7_PW8_vgg16.txt, então NOME_DO_ARQUIVO == _PW8_vgg16.txt
     f=open(file_name,'r')
     for lines in f:
         if str(lines) !='\n':
             lista.append(str(lines).split())
 
-    for i in range(len(lista)):
-        if i==0:
-            aux.append('teste'+str(j))
-        if i>=2 and i!= 8 and i<=9:
-            aux.append(lista[i][0])
+    if (str(args.pw) == '8' ):
+      for i in range(len(lista)):
+          if i==0:
+              aux.append('teste'+str(j))
+          if i>=2 and i!= 8 and i<=9:
+              aux.append(lista[i][0])
+    else:
+      for i in range(len(lista)):
+          if i==0:
+              aux.append('teste'+str(j))
+          if i>=2 and (not i in [8,9,10,11]) and i<=12:
+              aux.append(lista[i][0])
     append_list_as_row(file_output, aux)
     aux.clear()
     lista.clear()
