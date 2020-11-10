0502876f21d467acd0919936f306680b98339f02
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-11-10 09:57:10 -0300

    Update resnetClassification.py

--- ResNet/resnetClassification.py
+++ ResNet/resnetClassification.py
@@ -2,17 +2,21 @@ from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
-#import sys
-#lenght = int(sys.argv[2])
+import sys
+
 #folder = sys.argv[1]
 length = 731
+lenght = int(sys.argv[1])
+if (lenght > 731):
+    print("Using maximum length: 731")
+    lenght = 731
+again = int(sys.argv[2])
 folder_name = "hot_dog"
 
 model = ResNet50(weights='imagenet')
 
 images = []
 count = 0
-again = 10
 for i in range(length):
     img_path = folder_name + '/' + str(i) + '.jpg'  
     img = image.load_img(img_path, target_size=(224, 224))
