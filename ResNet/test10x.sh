aa2cdd7f51a221d84316a3a21e278928ac6a6568
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-11-15 19:19:40 -0300

    Update test10x.sh

--- ResNet/test10x.sh
+++ ResNet/test10x.sh
@@ -8,7 +8,7 @@ fi
 if [ "$1" = "pw9" ]; then
   mkdir -p ~/PW9/ResNet_hotdog
   mkdir -p ~/PW9/ResNet_hotdog/acc
-  for i in {0..9}; do
+  for i in {0..10}; do
     sudo sync;sudo bash -c "echo 3 > /proc/sys/vm/drop_caches"
     perf stat -o ~/PW9/ResNet_hotdog/N${i}_PW9_ResNet_hotdog.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions,idle-cycles-backend,idle-cycles-frontend python3 resnetClassification.py 500 1 >> ~/PW9/ResNet_hotdog/acc/PW9acc_hotdog${i}.txt
   done
@@ -20,7 +20,7 @@ fi
 if [ "$1" = "pw8" ]; then
   mkdir -p ~/PW8/ResNet_hotdog
   mkdir -p ~/PW8/ResNet_hotdog/acc
-  for i in {0..9}; do
+  for i in {0..10}; do
     sudo sync;sudo bash -c "echo 3 > /proc/sys/vm/drop_caches"
     perf stat -o ~/PW8/ResNet_hotdog/N${i}_PW8_ResNet_hotdog.txt -e branches,branch-misses,cache-misses,cache-references,cycles,instructions python3 resnetClassification.py 500 1 >> ~/PW8/ResNet_hotdog/acc/PW8_acc_hotdog${i}.txt
   done
